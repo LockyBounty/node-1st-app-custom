3bd079208871841b5a4b544e4cddb4024d8987b3
locky <inox127@hotmail.com>
2018-12-17 02:11:29 +0100

    lol2

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 module.exports = {
     'configName': 'node-auth',
     //'database' : 'mongodb://crow:crow123@ds225382.mlab.com:25382/diogo'
-    'database': 'mongodb://localhost/node-auth' // localmente
+	'database' : 'mongodb://locky:lockytest2@ds151012.mlab.com:51012/test-app-becode-locky'
+    //'database': 'mongodb://localhost/node-auth' // localmente
 }
